c6f93a3b1932c59d376730d309f5618d9c41ac09
oomusou <oomusou@gmail.com>
2016-05-25 22:45:50 +0800

    新增 Model Factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,9 +13,26 @@
 
 $factory->define(App\User::class, function (Faker\Generator $faker) {
     return [
-        'name' => $faker->name,
-        'email' => $faker->safeEmail,
-        'password' => bcrypt(str_random(10)),
+        'name'           => $faker->name,
+        'email'          => $faker->safeEmail,
+        'password'       => bcrypt(str_random(10)),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(App\Post::class, function (Faker\Generator $faker) {
+    return [
+        'title'       => $faker->sentences,
+        'description' => $faker->sentences,
+        'content'     => $faker->paragraphs,
+    ];
+});
+
+
+$factory->define(App\Comment::class, function (Faker\Generator $faker) {
+    return [
+        'name'    => $faker->name,
+        'email'   => $faker->safeEmail,
+        'comment' => $faker->paragraphs,
     ];
 });
