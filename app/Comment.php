8c8541d20000a14de0cf81a7b89d1cb2a4bea83a
oomusou <oomusou@gmail.com>
2016-05-25 22:55:11 +0800

    新增 Model 註解

--- app/Comment.php
+++ app/Comment.php
@@ -4,6 +4,25 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Comment
+ *
+ * @property integer $id
+ * @property integer $post_id
+ * @property string $name
+ * @property string $email
+ * @property string $comment
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment wherePostId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereName($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereEmail($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereComment($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Comment whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Comment extends Model
 {
     protected $fillable = [
--- app/Post.php
+++ app/Post.php
@@ -4,6 +4,23 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Post
+ *
+ * @property integer $id
+ * @property string $title
+ * @property string $description
+ * @property string $content
+ * @property \Carbon\Carbon $created_at
+ * @property \Carbon\Carbon $updated_at
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereId($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereTitle($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereDescription($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereContent($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereCreatedAt($value)
+ * @method static \Illuminate\Database\Query\Builder|\App\Post whereUpdatedAt($value)
+ * @mixin \Eloquent
+ */
 class Post extends Model
 {
     protected $fillable = [
