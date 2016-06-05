6e0f9604413f8fa152fab40069c83c794de74f9d
oomusou <oomusou@gmail.com>
2016-06-05 10:35:47 +0800

    [單元測試] 先新增Post再新增多筆Comment

--- tests/Unit/Repositories/PostRepositoryTest.php
+++ tests/Unit/Repositories/PostRepositoryTest.php
@@ -36,4 +36,45 @@ class PostRepositoryTest extends TestCase
         $this->seeInDatabase('posts', $expectedPost);
         $this->seeInDatabase('comments', $expectedComment);
     }
+
+    /** @test */
+    public function 先新增Post再新增多筆Comment()
+    {
+        /** arrange */
+        //<editor-fold desc="Expected">
+        $expectedPost = [
+            'title'       => 'Post1 title',
+            'description' => 'Post1 description',
+            'content'     => 'Post1 content'
+        ];
+
+        $expectedComment = [
+            [
+                'name'    => 'Sam',
+                'email'   => 'oomusou@gmail.com',
+                'comment' => "Sam's comment"
+            ],
+            [
+                'name'    => 'Sunny',
+                'email'   => 'sunny@gmail.com',
+                'comment' => "Sunny's comment"
+            ],
+        ];
+        //</editor-fold>
+
+        $target = app(PostRepository::class);
+
+        /** act */
+        $post = $target->create($expectedPost);
+
+        collect($expectedComment)
+            ->each(function ($value) use ($post) {
+                $post->comments()->create($value);
+            });
+
+        /** assert */
+        $this->seeInDatabase('posts', $expectedPost);
+        $this->seeInDatabase('comments', $expectedComment[0]);
+        $this->seeInDatabase('comments', $expectedComment[1]);
+    }
 }
